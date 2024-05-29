bc7f14d681c95c9efbcdcb997405a3f5c48e15af
Thayna Santana <45610362+thaynamenezes@users.noreply.github.com>
2024-05-28 18:16:15 -0700

    Pause menu pop up and hover effect working

--- Assets/Scripts/Camera/MouseLook.cs
+++ Assets/Scripts/Camera/MouseLook.cs
@@ -27,6 +27,8 @@ public class MouseLook : MonoBehaviour
 
     private void Update()
     {
+        if (PauseMenu.GameIsPaused) return; // Do nothing if the game is paused
+
         MyInput();
 
         cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, wallRun.tilt);
@@ -35,6 +37,8 @@ public class MouseLook : MonoBehaviour
 
     void MyInput()
     {
+        if (PauseMenu.GameIsPaused) return; // Do nothing if the game is paused
+
         mouseX = Input.GetAxisRaw("Mouse X");
         mouseY = Input.GetAxisRaw("Mouse Y");
 
